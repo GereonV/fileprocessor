b9789d98477a886be9a21a8fee96ada52b62e8dc
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-05-08 00:32:11 +0200

    Remove Mode-Functionality from FileProcessor
    Previously the desired Line-Feed of the Output-File had to be specified.
    This change drops this requirement and also deprecates it's use by
    making it an invalid Option.
    In it's place now the platform-default will be used.
    The Mode of Input-Files is still infered from their respective contents.

--- FileProcessor/src/main.c
+++ FileProcessor/src/main.c
@@ -11,33 +11,15 @@ typedef struct {
 } PATH;
 
 typedef enum {
-    MODE_NONE,
-    MODE_LF,
-    MODE_CRLF
-} MODE;
-typedef const struct {
-    size_t length;
-    const char *string;
-} LF;
-static LF lineFeeds[] = {
-    { .length = 1, .string = "\n" },
-    { .length = 2, .string = "\r\n" }
-};
-
-typedef enum {
     ARGUMENT_VALID,
     ARGUMENT_TOO_LONG,
     ARGUMENT_OUTPUT_REDEFINED,
-    ARGUMENT_MODE_INVALID,
-    ARGUMENT_MODE_REDEFINED,
     ARGUMENT_OPTION_INVALID,
     ARGUMENT_HELP
 } ARGUMENT;
 static const char *ERRORS_ARGUMENT[] = {
     "too long",
     "Output already defined",
-    "not a valid Mode",
-    "Mode already defined",
     "not a valid Option"
 };
 
@@ -45,17 +27,14 @@ typedef ARGUMENT (*ARG_FUNC)(const char *, size_t);
 
 typedef enum {
     STATE_VALID,
-    STATE_OUTPUT_UNDEFINED,
-    STATE_MODE_UNDEFINED
+    STATE_OUTPUT_UNDEFINED
 } STATE;
 static const char *ERRORS_STATE[] = {
     "no Output provided",
-    "no Mode provided"
 };
 
 static size_t inputCount;
 static PATH outputPath, *inputPaths, directoryPath = { .length = 2, .string = "."};
-static MODE mode;
 
 static void toScreamingSnakeCase(char *const string, const size_t length) {
     for(size_t i = 0; i < length; i++)
@@ -81,19 +60,6 @@ static ARGUMENT handleDirectory(const char *const directory, size_t length) {
     return ARGUMENT_VALID;
 }
 
-static ARGUMENT handleMode(const char *const modeStr, const size_t length) {
-    if(mode)
-        return ARGUMENT_MODE_REDEFINED;
-    char *string = strdup(modeStr);
-    toScreamingSnakeCase(string, length);
-    if(!strcmp(string, "LF"))
-        mode = MODE_LF;
-    else if(!strcmp(string, "CRLF"))
-        mode = MODE_CRLF;
-    free(string);
-    return mode ? ARGUMENT_VALID : ARGUMENT_MODE_INVALID;
-}
-
 static inline ARGUMENT handleOption(const char *const option, const size_t length) {
     ARG_FUNC func;
     switch(option[0]) {
@@ -102,9 +68,6 @@ static inline ARGUMENT handleOption(const char *const option, const size_t lengt
             break;
         case 'D':
             func = handleDirectory;
-            break;
-        case 'M':
-            func = handleMode;
             break;
         default:
             return ARGUMENT_OPTION_INVALID;
@@ -141,22 +104,20 @@ static inline ARGUMENT handleArgument(const char *const argument) {
 static inline STATE checkState() {
     if(!*outputPath.string)
         return STATE_OUTPUT_UNDEFINED;
-    if(!mode)
-        return STATE_MODE_UNDEFINED;
     return STATE_VALID;
 }
 
-static void outputFormatted(const char *const name, char *const buffer, FILE *const outFile, LF *const outNewLine) {
+static void outputFormatted(const char *const name, char *const buffer, FILE *const outFile) {
     const char *inNewLine = strchr(buffer, '\r') ? "\r\n" : "\n";
     size_t inNewLineLength = strlen(inNewLine);
     fprintf(outFile, "#define %s", name);
     char *line = buffer, *lineEnd;
     while(lineEnd = strstr(line, inNewLine)) {
         *lineEnd = 0;
-        fprintf(outFile, " \\%s\"%s\\n\"", outNewLine->string, line);
+        fprintf(outFile, " \\\n\"%s\\n\"", line);
         line = lineEnd + inNewLineLength;
     }
-    fprintf(outFile, " \\%s\"%s\"%s", outNewLine->string, line, outNewLine->string);
+    fprintf(outFile, " \\\n\"%s\"\n", line);
 }
 
 int main(const int argc, const char *const argv[]) {
@@ -188,12 +149,11 @@ int main(const int argc, const char *const argv[]) {
         ERR("Invalid State: %s\n", ERRORS_STATE[state - 1]);
         return -1;
     }
-    FILE *outFile = fopen(outputPath.string, "wb");
+    FILE *outFile = fopen(outputPath.string, "w");
     if(!outFile) {
         ERR("Output-File (%s) couldn't be opened\n", outputPath.string);
         return -1;
     }
-    LF *outNewLine = lineFeeds + mode - 1;
     for(size_t i = 0; i < inputCount; i++) {
         FILE *inFile = fopen(inputPaths[i].string, "rb");
         if(!inFile) {
@@ -208,7 +168,7 @@ int main(const int argc, const char *const argv[]) {
         buffer[length] = 0;
         char *filename = strrchr(inputPaths[i].string, '/') + 1;
         toScreamingSnakeCase(filename, inputPaths[i].length - (filename - inputPaths[i].string) - 1);
-        outputFormatted(filename, buffer, outFile, outNewLine);
+        outputFormatted(filename, buffer, outFile);
         free(buffer);
         fclose(inFile);
     }
